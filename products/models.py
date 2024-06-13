acfcff083c4dd92050734dc2d3b0494e19586329
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-06-13 12:59:02 +0200

    prepare pics for products. make the product model

---
+++ products/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    friendly_name = models.CharField(max_length=255, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+    weight = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
