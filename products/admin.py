65f403c16a0bea097a72a612370b2c7574c74f3c
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-06-13 13:03:51 +0200

    update admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Category, Product
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'is_available',
+        'weight',
+    )
+    
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
