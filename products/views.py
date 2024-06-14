64f1b6cb047be29f08baf04c364687bd63b4d156
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-06-14 10:44:30 +0200

    fix problem with filtering

--- products/views.py
+++ products/views.py
@@ -30,8 +30,8 @@ def all_products(request):
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__slug__in=categories)
-            categories = Category.objects.filter(slug__in=categories)
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
             
         if 'q' in request.GET:
             query = request.GET['q']
