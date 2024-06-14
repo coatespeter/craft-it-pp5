80391cafca132687ff826d5e145973fcbb29a6ba
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-06-14 12:44:38 +0200

    add add to bag functionality

--- products/models.py
+++ products/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 
-# Create your models here.
-
 class Category(models.Model):
-
     class Meta:
         verbose_name_plural = 'Categories'
         
     name = models.CharField(max_length=255)
     friendly_name = models.CharField(max_length=255, null=True, blank=True)
-
+    slug = models.SlugField(unique=True, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.friendly_name if self.friendly_name else self.name
     
     def get_friendly_name(self):
         return self.friendly_name
     
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=32, unique=True, null=True, blank=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -26,6 +24,7 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
     is_available = models.BooleanField(default=True)
     weight = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
