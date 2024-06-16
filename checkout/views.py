66f1a9fa73e95b8af01a5bdd11f4dff4434c617a
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-06-16 14:11:25 +0200

    update checkout and settings

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -10,6 +13,9 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag right now")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
