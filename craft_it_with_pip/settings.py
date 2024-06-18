f5353b798b5d642cb022ae58ae33f16a37ef6c25
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-06-18 12:17:41 +0200

    middleware fix. deploy working here

--- craft_it_with_pip/settings.py
+++ craft_it_with_pip/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
 ]
 
 ROOT_URLCONF = 'craft_it_with_pip.urls'
