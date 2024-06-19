106ae2bffde19edd5c0c68cc1bbb8fbb563232ff
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-06-19 09:35:12 +0200

    update models to remove updated at field

--- interactions/models.py
+++ interactions/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class FAQ(models.Model):
     question = models.CharField(max_length=255)
     answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.question
@@ -13,8 +12,7 @@ class FAQ(models.Model):
 class Testimonial(models.Model):
     name = models.CharField(max_length=255)
     text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f"Testimonial from {self.name}"
